2eb029b6885c4de54b8424afdac12cc76fa317fc
Lisa Felberg <lfelberg@berkeley.edu>
2014-08-07 15:01:06 -0700

    BDmulti work

--- src/BDmulti.cpp
+++ src/BDmulti.cpp
@@ -91,142 +91,132 @@ void
 CBDmulti::initMolTypes(  const vector<char*> &molfnames1,
 											 const vector<char*> &molfnames2, REAL idiel  )
 {
-	// Storing the names of various files for
-	// each molecule
-	char* pqrfile1  = molfnames1[0];	// PQR
-	char* imatpath1 = molfnames1[1];	// Imat
-	char* exppath1  = molfnames1[2];	// Self pole path
-	char* expname1  = molfnames1[3];	// Selfpole name
-	char* pqrfile2  = molfnames2[0];	// PQR
-	char* imatpath2 = molfnames2[1];  // Imat
-	char* exppath2  = molfnames2[2];  // Self pole path
-	char* expname2  = molfnames2[3];  // Selfpole name
+  char* pqrfile1  = molfnames1[0];
+  char* imatpath1 = molfnames1[1];
+  char* exppath1  = molfnames1[2];
+  char* expname1  = molfnames1[3];
 	
-	// read in charge + centers
-	vector<CPnt> scen1, POS1, scen2, POS2;		// Centers and positions
-	vector<double> srad1, CHG1,  srad2, CHG2;	// radius and charges
-	readpqr(  pqrfile1, POS1, CHG1 ,srad1, scen1  );
-	readpqr(  pqrfile2, POS2, CHG2 ,srad2, scen2  );
+  char* pqrfile2  = molfnames2[0];
+  char* imatpath2 = molfnames2[1];
+  char* exppath2  = molfnames2[2];
+  char* expname2  = molfnames2[3];
 	
-	// Something about the size of centers?
-	const int ncen1 = scen1.size(    );
-	const int ncen2 = scen2.size(    );
+  // read in charge + centers
+  vector<CPnt> scen1, POS1, scen2, POS2;
+  vector<double> srad1, CHG1,  srad2, CHG2;
+  readpqr(pqrfile1, POS1, CHG1 ,srad1, scen1);
+  readpqr(pqrfile2, POS2, CHG2 ,srad2, scen2);
 	
-	// Setting it to zero intially
-	m_initialrcen1 = CPnt(  0,0,0  ), m_initialrcen2 = CPnt(0,0,0);
+  const int ncen1 = scen1.size();
+  const int ncen2 = scen2.size();
 	
-	// Computing the center of mass of each molecule
-	for(  int i=0; i<ncen1; i++  )
-		m_initialrcen1 += scen1[i]; m_initialrcen1 /= scen1.size(    );
-	for(  int i=0; i<ncen2; i++  )
-		m_initialrcen2 += scen2[i]; m_initialrcen2 /= scen2.size(    );
+  m_initialrcen1 = CPnt(0,0,0), m_initialrcen2 = CPnt(0,0,0);
+  for(int i=0; i<ncen1; i++) m_initialrcen1 += scen1[i]; m_initialrcen1 /= scen1.size();
+  for(int i=0; i<ncen2; i++) m_initialrcen2 += scen2[i]; m_initialrcen2 /= scen2.size();
+  cout <<"initialrcen1 : " <<m_initialrcen1<<endl;
+  cout <<"initialrcen2 : " <<m_initialrcen2<<endl;
+  
 	
-	// Printing the center of mass
-	cout <<"initialrcen1 : " <<m_initialrcen1<<endl;
-	cout <<"initialrcen2 : " <<m_initialrcen2<<endl;
-	//////////////// read in solved expansions //////////////
-	readMats(  m_iMats1, N_POLES, imatpath1, ncen1  );
-	readMats(  m_iMats2, N_POLES, imatpath2, ncen2  );
-	REAL intraRcutoff_dum;
-	readExpansionsP(  m_iF1, m_iH1, N_POLES, exppath1,
-									expname1, ncen1, intraRcutoff_dum  );
-	readExpansionsP(  m_iF2, m_iH2, N_POLES, exppath2,
-									expname2, ncen2, intraRcutoff_dum  );
+  //////////////// read in solved expansions //////////////
+  readMats(m_iMats1, N_POLES, imatpath1, ncen1);
+  readMats(m_iMats2, N_POLES, imatpath2, ncen2);
+  REAL intraRcutoff_dum;
+  readExpansionsP(m_iF1, m_iH1, N_POLES, exppath1, expname1, ncen1, intraRcutoff_dum);
+  readExpansionsP(m_iF2, m_iH2, N_POLES, exppath2, expname2, ncen2, intraRcutoff_dum);
 	
-	/////////////// generate values for each moltype ///////////////
-	const double intraRcutoff = 30;  //<<<<<<<<
-	vector< vector<int> > intraPolLists_far1, intraPolLists_far2;
+  /////////////// generate values for each moltype ///////////////
+  const double intraRcutoff = 30;  //<<<<<<<<
+  vector< vector<int> > intraPolLists_far1, intraPolLists_far2;
 	
-	// protein 1
-	// 'generateMolSPX' will generate surface points ( S. Liu )
-	CMolecule::generateMolSPX(  scen1, srad1, m_SPxes1, m_nSPx1, m_neighs1   );
-	//'generateMolExposedChargesFH' will calculate surface charge on each sphere
-	// represented by multipole expansions of F and H ( S. Liu )
-	CMolecule::generateMolExposedChargesFH(  m_iF1, m_iH1, scen1, srad1, m_SPxes1, m_nSPx1,
-																				 m_qSolvedF1, m_qSolvedH1, m_totalF1, m_totalH1  );
-	CMolecule::generateMolCells(  scen1, srad1, m_initialrcen1, m_molcells1   );
-	//'generateMolTypeIntraPolLists' will generate lists for
-	//intra-molecular polarization ( S. Liu )
-	CMolecule::generateMolTypeIntraPolLists(  scen1, srad1, intraRcutoff,
-																					m_intraPolLists_near1, intraPolLists_far1  );
-	//see "molecule.cpp" for interpretations of 'computeMolTypeValues'  ( S. Liu )
-	CMolecule::computeMolTypeValues(  m_initialrcen1, scen1, srad1, CHG1, POS1, idiel,
-																	intraRcutoff,
-																	m_SPxes1, m_nSPx1, m_neighs1,
-																	m_iF1, m_iH1, m_qSolvedF1, m_qSolvedH1, m_totalF1, m_totalH1,
-																	m_intraPolLists_near1,
-																	intraPolLists_far1,
-																	m_LFs_intraSelf1, m_LHs_intraSelf1,
-																	m_LFs_intraSelf_far1, m_LHs_intraSelf_far1  );
+  // protein 1
+  //'generateMolSPX' will generate surface points (S. Liu)
+  CMolecule::generateMolSPX(scen1, srad1, m_SPxes1, m_nSPx1, m_neighs1 );
+  //'generateMolExposedChargesFH' will calculate surface charge on each sphere
+  // represented by multipole expansions of F and H (S. Liu)
+  CMolecule::generateMolExposedChargesFH(m_iF1, m_iH1, scen1, srad1, m_SPxes1, m_nSPx1,
+                                         m_qSolvedF1, m_qSolvedH1, m_totalF1, m_totalH1);
+  CMolecule::generateMolCells(scen1, srad1, m_initialrcen1, m_molcells1 );
 	
-	// protein 2
-	CMolecule::generateMolSPX(  scen2, srad2, m_SPxes2, m_nSPx2, m_neighs2   );
-	CMolecule::generateMolExposedChargesFH(  m_iF2, m_iH2, scen2, srad2, m_SPxes2, m_nSPx2,
-																				 m_qSolvedF2, m_qSolvedH2, m_totalF2, m_totalH2  );
-	CMolecule::generateMolCells(  scen2, srad2, m_initialrcen2, m_molcells2   );
-	CMolecule::generateMolTypeIntraPolLists(  scen2, srad2, intraRcutoff,
-																					m_intraPolLists_near2, intraPolLists_far2  );
-	CMolecule::computeMolTypeValues(  m_initialrcen2, scen2, srad2, CHG2, POS2, idiel,
-																	intraRcutoff,
-																	m_SPxes2, m_nSPx2, m_neighs2,
-																	m_iF2, m_iH2, m_qSolvedF2, m_qSolvedH2, m_totalF2, m_totalH2,
-																	m_intraPolLists_near2,
-																	intraPolLists_far2,
-																	m_LFs_intraSelf2, m_LHs_intraSelf2,
-																	m_LFs_intraSelf_far2, m_LHs_intraSelf_far2  );
+  //'generateMolTypeIntraPolLists' will generate lists for intra-molecular polarization (S. Liu)
+  CMolecule::generateMolTypeIntraPolLists(scen1, srad1, intraRcutoff,
+                                          m_intraPolLists_near1, intraPolLists_far1);
+  //see "molecule.cpp" for interpretations of 'computeMolTypeValues'  (S. Liu)
+  CMolecule::computeMolTypeValues(m_initialrcen1, scen1, srad1, CHG1, POS1, idiel,
+                                  intraRcutoff,
+                                  m_SPxes1, m_nSPx1, m_neighs1,
+                                  m_iF1, m_iH1, m_qSolvedF1, m_qSolvedH1, m_totalF1, m_totalH1,
+                                  m_intraPolLists_near1,
+                                  intraPolLists_far1,
+                                  m_LFs_intraSelf1, m_LHs_intraSelf1,
+                                  m_LFs_intraSelf_far1, m_LHs_intraSelf_far1);
 	
-	// create molecules
-	m_mols.clear(    );
+  // protein 2
+  CMolecule::generateMolSPX(scen2, srad2, m_SPxes2, m_nSPx2, m_neighs2 );
+  CMolecule::generateMolExposedChargesFH(m_iF2, m_iH2, scen2, srad2, m_SPxes2, m_nSPx2,
+                                         m_qSolvedF2, m_qSolvedH2, m_totalF2, m_totalH2);
+  CMolecule::generateMolCells(scen2, srad2, m_initialrcen2, m_molcells2 );
 	
-	for(  int i=0; i < m_np1; i++  )
+  CMolecule::generateMolTypeIntraPolLists(scen2, srad2, intraRcutoff,
+                                          m_intraPolLists_near2, intraPolLists_far2);
+	
+  CMolecule::computeMolTypeValues(m_initialrcen2, scen2, srad2, CHG2, POS2, idiel,
+                                  intraRcutoff,
+                                  m_SPxes2, m_nSPx2, m_neighs2,
+                                  m_iF2, m_iH2, m_qSolvedF2, m_qSolvedH2, m_totalF2, m_totalH2,
+                                  m_intraPolLists_near2,
+                                  intraPolLists_far2,
+                                  m_LFs_intraSelf2, m_LHs_intraSelf2,
+                                  m_LFs_intraSelf_far2, m_LHs_intraSelf_far2);
+	
+  // create molecules
+  m_mols.clear();
+  
+  for(int i=0; i < m_np1; i++)
 	{
-		m_mols.push_back(   new CMolecule(0, m_initialrcen1, scen1, srad1, CHG1, POS1, idiel,
-																			m_iMats1, intraRcutoff,
-																			m_SPxes1, m_nSPx1, m_neighs1, m_intraPolLists_near1,
-																			m_iF1, m_iH1, m_LFs_intraSelf1, m_LHs_intraSelf1,
-																			m_LFs_intraSelf_far1, m_LHs_intraSelf_far1,
-																			m_qSolvedF1, m_qSolvedH1, m_totalF1, m_totalH1, m_molcells1  ));
-	}
-	for(  int i=0; i < m_np2; i++  )
-	{
-		m_mols.push_back(   new CMolecule(1, m_initialrcen2, scen2, srad2, CHG2, POS2, idiel,
-																			m_iMats2, intraRcutoff,
-																			m_SPxes2, m_nSPx2, m_neighs2, m_intraPolLists_near2,
-																			m_iF2, m_iH2, m_LFs_intraSelf2, m_LHs_intraSelf2,
-																			m_LFs_intraSelf_far2, m_LHs_intraSelf_far2,
-																			m_qSolvedF2, m_qSolvedH2, m_totalF2, m_totalH2, m_molcells2  ));
+		m_mols.push_back( new CMolecule(0, m_initialrcen1, scen1, srad1, CHG1, POS1, idiel,
+																		m_iMats1, intraRcutoff,
+																		m_SPxes1, m_nSPx1, m_neighs1, m_intraPolLists_near1,
+																		m_iF1, m_iH1, m_LFs_intraSelf1, m_LHs_intraSelf1,
+																		m_LFs_intraSelf_far1, m_LHs_intraSelf_far1,
+																		m_qSolvedF1, m_qSolvedH1, m_totalF1, m_totalH1, m_molcells1));
 	}
 	
-	// Check to make sure the number of spheres is correct
-	assert(  m_mols.size(  ) == m_np1+m_np2);
-	// Display which molecule indices belong to which moltype
-	if(  m_np1 > 0  ) cout << "index 0 to " <<m_np1-1<<" are moltype 0"<<endl;
-	if(  m_np2 > 0  )
-		cout << "index "<<m_np1<<" to "<<m_np1+m_np2-1<<" are moltype 1"<<endl;
+  for(int i=0; i < m_np2; i++)
+	{
+		m_mols.push_back( new CMolecule(1, m_initialrcen2, scen2, srad2, CHG2, POS2, idiel,
+																		m_iMats2, intraRcutoff,
+																		m_SPxes2, m_nSPx2, m_neighs2, m_intraPolLists_near2,
+																		m_iF2, m_iH2, m_LFs_intraSelf2, m_LHs_intraSelf2,
+																		m_LFs_intraSelf_far2, m_LHs_intraSelf_far2,
+																		m_qSolvedF2, m_qSolvedH2, m_totalF2, m_totalH2, m_molcells2));
+	}
 	
-	// =========================================================
-	// "interRCutoff", which gives the value of "m_interRCutoff" in class "CMolecule",
-	// as you will see in function CMolecule::generateInterXFormsForPolarize_LowMemory,
-	// if the distance between sphere i in one molecule and sphere j is less than
-	// 'interRCutoff', we will not polarize the gradients of their expansions, which
-	// will be used for force calculation ( S. Liu )
-	const REAL interRCutoff = 10; // <<<<<<<<<<<<<<<<<<<<<<<<
-	// interactRCutoff, which gives the valule of "m_interactRCutoff" in class CMolecule
-	// is the cutoff radius for doing mutual polarization, i.e., if the distance
-	// between two molecules is greater than interacRCutoff, this pair won't be mutually
-	// polarized ( S. Liu )
-	const REAL interactRCutoff = 100; // <<<<<<<<<<<<<<<<<<<<<<<<
-	CMolecule::initMutualConstants(  m_mols, interRCutoff, interactRCutoff, true   );
-	cout << "interRcutoff = "<<interRCutoff<<endl;
-	cout << "interactRcutoff = "<<interactRCutoff<<endl;
-	// =========================================================
-	
-	// initialize diffusion parameters
-	m_Dtr.resize(  m_nMolType  );
-	m_Dr.resize(  m_nMolType  );
-	// Diffusion parameters for 1BRS!!!
-	m_Dtr[0] = 0.015; m_Dtr[1] = 0.015; // 1BRS values, translational diffusion coefficient
-	m_Dr[0] = 4.5e-5; m_Dr[1] = 4e-5;	// rotational diffusion coefficient ( S. Liu )
+  assert(m_mols.size() == m_np1+m_np2);
+  if(m_np1 > 0) cout << "index 0 to " <<m_np1-1<<" are moltype 0"<<endl;
+  if(m_np2 > 0) cout << "index "<<m_np1<<" to "<<m_np1+m_np2-1<<" are moltype 1"<<endl;
+  // =========================================================
+  //"interRCutoff", which gives the value of "m_interRCutoff" in class "CMolecule",
+  //as you will see in function CMolecule::generateInterXFormsForPolarize_LowMemory,
+  // if the distance between sphere i in one molecule and sphere j is less than
+  // 'interRCutoff', we will not polarize the gradients of their expansions, which
+  // will be used for force calculation (S. Liu)
+  const REAL interRCutoff = 10; // <<<<<<<<<<<<<<<<<<<<<<<<
+  //interactRCutoff, which gives the valule of "m_interactRCutoff" in class CMolecule
+  // is the cutoff radius for doing mutual polarization, i.e., if the distance
+  //between two molecules is greater than interacRCutoff, this pair won't be mutually
+  //polarized (S. Liu)
+  const REAL interactRCutoff = 100; // <<<<<<<<<<<<<<<<<<<<<<<<
+  CMolecule::initMutualConstants(m_mols, interRCutoff, interactRCutoff, true );
+  cout << "interRcutoff = "<<interRCutoff<<endl;
+  cout << "interactRcutoff = "<<interactRCutoff<<endl;
+  // =========================================================
+  
+  // initialize diffusion parameters
+  m_Dtr.resize(m_nMolType);
+  m_Dr.resize(m_nMolType);
+  
+  m_Dtr[0] = 0.015; m_Dtr[1] = 0.015; // 1BRS values, translational diffusion coefficient
+  m_Dr[0] = 4.5e-5; m_Dr[1] = 4e-5;//rotational diffusion coefficient (S. Liu)
 	return;
 } // end initMolTypes
 
--- src/BDmulti_main.cpp
+++ src/BDmulti_main.cpp
@@ -27,146 +27,116 @@
  ******************************************************************/
 int bd_multi( int argc, char ** argv )
 {
-	int num_threads = atoi( argv[1] );			// Num threads OMP
-	const double salt_conc = atof( argv[2] );	// Salt concentration
-	const int ntraj = atoi( argv[3] );			// Number of trajectories
-	const double consepdist = atof( argv[4] );	// Separation distance [Angstrom]
-	const int np1 = atoi( argv[5] );			// Protein 1
-	const int np2 = atoi( argv[6] );			// Protein 2
-	const double boxlength = atof( argv[7] );	// Length of sim box [Angstr]
-	const char* contactfile = argv[8];			// Contact file name
-	const double maxtime = atof( argv[9] );		// Max time for simulation
-	char* runname = argv[10];					// Name desired for run
-	const int systemIndex  = atoi( argv[11] );	// Not realy sure
+  int num_threads = atoi(argv[1]);
+  const double salt_conc = atof(argv[2]);
+  const int ntraj = atoi(argv[3]);
+  const double consepdist = atof(argv[4]);
+  const int np1 = atoi(argv[5]);
+  const int np2 = atoi(argv[6]);
+  const double boxlength = atof(argv[7]);
+  const char* contactfile = argv[8];
+  const double maxtime = atof(argv[9]);
+  char* runname = argv[10];
+  const int systemIndex  = atoi(argv[11]);
 	
-	for( int i=0; i<argc; i++ )
-		cout <<argv[i]<<" " << endl;
-	cout<<"variables read"<<endl;
-	
-	// Generating random number seed
-	cout.precision( 5 );
-	long int idum=( unsigned )time(NULL);
-	srand48( idum );
-	cout <<"Random Seed = "<<idum<<endl;
-	// Defining number of threads for OMP
+  for(int i=0; i<argc; i++) cout <<argv[i]<<" "; cout<<"variables read"<<endl;
+  cout.precision(5);
+  long int idum=(unsigned)time(NULL);
+  srand48(idum);  //this will initialize random number generator for BD simulation (S. Liu)
+  cout <<"Random Seed = "<<idum<<endl;
 #ifdef __OMP
-	omp_set_num_threads( num_threads );
+  omp_set_num_threads(num_threads);
 #endif
-	// system parameters NEED UNITSSS
-	const double idiel = 4.0;
-	const double sdiel = 78.0;
-	const double temperature = 298.15;		// Kelvin
-	const double kappa = ANGSTROM * sqrt(   (2 * salt_conc * AVOGADRO_NUM / LITRE
-																					 * ELECT_CHG * ELECT_CHG )
-																			 / ( sdiel* PERMITTIVITY_VAC * KB * temperature  ) );
+  // system parameters
+  const double idiel = 4.0;
+  const double sdiel = 78.0;
+  const double temperature = 298.15;
+  const double kappa = ANGSTROM * sqrt(  (2 * salt_conc * AVOGADRO_NUM / LITRE
+																					* ELECT_CHG * ELECT_CHG)
+																			 / (sdiel* PERMITTIVITY_VAC * KB * temperature ) );
 	
-	cout <<"EPS_SOLVENT:"<<sdiel<<" EPS_IN: "<<idiel<<" KAPPA: "<<kappa<<endl;
-	// printing out the number of each molecule and the boxlength in angstroms
-	cout <<" Np1 ="<<np1<<" Np2 = "<<np2<<" Boxlength [A] ="<<boxlength<<endl;
-	// computes the concentration by dividing the # mol1/ boxlen^3
-	cout <<" -> Species 0 Paritial Concentration: "<<np1/pow( boxlength,3.0 )
-	<<" [A-3]"<<endl;
-	// computes the concentration by dividing the # mol2/ boxlen^3
-	cout <<" -> Species 1 Paritial Concentration: "<<np2/pow( boxlength,3.0 )
-	<<" [A-3]"<<endl;
+  cout <<"EPS_SOLVENT:"<<sdiel<<" EPS_IN: "<<idiel<<" KAPPA: "<<kappa<<endl;
+  cout <<" Np1 ="<<np1<<" Np2 = "<<np2<<" Boxlength ="<<boxlength<<endl;
+  cout <<" -> Species 0 Paritial Concentration: "<<np1/pow(boxlength,3.0)<<" [A-3]"<<endl;
+  cout <<" -> Species 1 Paritial Concentration: "<<np2/pow(boxlength,3.0)<<" [A-3]"<<endl;
 	
-	// Initiating system constants "true" means here we are using periodic boundary
-	// condition (S. Liu)
-	CSystem::initConstants( kappa, sdiel, temperature, true, boxlength );
-	// Initializing the class BDmulti. nmoltype is the number of different molecule types
-	// for now it is set at two
-	const int nmoltype=2;
-	CBDmulti::initConstants( nmoltype );
-	// Set separation distance and read in contacts
-	CContact::setSepDist( consepdist );
-	vector<CMolContact> molcontactlist;
-	// Contact information
-	int moltype;
-	CMolContact::readMolContact( contactfile, moltype, molcontactlist );
-	assert ( moltype == 0 ); // since only two species, we always
-	// check species 0's contacts
-	CBDmulti::addMolContact( moltype, molcontactlist );
+  //initialize constants of the system, "true" means here we are using periodic boundary
+  //condition (S. Liu)
+  CSystem::initConstants(kappa, sdiel, temperature, true, boxlength);
+  const int nmoltype=2;
+  CBDmulti::initConstants(nmoltype);
+  // read in contacts
+  CContact::setSepDist(consepdist);
+  vector<CMolContact> molcontactlist;
 	
-	// create molecules
-	vector<char*> molfnames1, molfnames2;
-	molfnames1.resize( 4 );
-	molfnames1[0] = argv[12];	//pqr file
-	molfnames1[1] = argv[13]; 	//Imat path
-	molfnames1[2] = argv[14]; 	//expansion path
-	molfnames1[3] = argv[15]; 	//expname
-	molfnames2.resize( 4 );
-	molfnames2[0] = argv[16];  	//pqr file
-	molfnames2[1] = argv[17]; 	//Imat path
-	molfnames2[2] = argv[18]; 	//expansion path
-	molfnames2[3] = argv[19]; 	//expname
+  int moltype;
+  CMolContact::readMolContact(contactfile, moltype, molcontactlist);
+  assert (moltype == 0); // since only two species, we always check species 0's contacts
+  CBDmulti::addMolContact(moltype, molcontactlist);
+  
+  // create molecules
+  vector<char*> molfnames1, molfnames2;
+	molfnames1.resize(4);
+	molfnames1[0] = argv[12];  //pqr file
+	molfnames1[1] = argv[13]; //Imat path
+	molfnames1[2] = argv[14]; //expansion path
+	molfnames1[3] = argv[15]; //expname
+	molfnames2.resize(4);
+	molfnames2[0] = argv[16];  //pqr file
+	molfnames2[1] = argv[17]; //Imat path
+	molfnames2[2] = argv[18]; //expansion path
+	molfnames2[3] = argv[19]; //expname
 	
-	// create BD system
-	bool bRestart;
-	char* configfname;
-	CBDmulti* bd;		// "bd" points to an object of class CBDmulti (S. Liu)
-	
-	// If there are only 13 arguments, this means that the program
-	// is starting from a restart file
-	if( argc==13 )
+  // create BD system
+  bool bRestart;
+  char* configfname;
+  //"bd" is a pointer that points to an object of class CBDmulti (S. Liu)
+  CBDmulti* bd;
+  
+  if(argc==13)
 	{
+		//restart from an existing configuration file (S. Liu)
 		bRestart = true;
 		configfname = argv[12];
 		cout <<"Initializing from restart file "<<configfname<<endl;
 	}
-	// Otherwise, restart from an initial configuration where all
-	// molecules are placed on a lattice (S. Liu)
-	else
+  else
 	{
+		//restart from an initial configuration where all molecules are placed on a lattice (S. Liu)
 		bRestart = false;
 		cout <<"Initializing from lattice "<<endl;
 	}
-	// create "bd" with a constructor
-	bd = new CBDmulti( np1, np2, molfnames1,molfnames2, idiel );
-	// start BD
-	char outfname[MAX_CHARNUM];
-	// Output to go to mfpt.[runname].dat
-	sprintf( outfname, "mfpt.%s.dat", runname );
-	ofstream fout( outfname );
-	// Starting timer
-	double start = read_timer(  );
-	int cdock = 0;
-	
-	// For the given number of trajectories
-	for ( int r=0; r<ntraj; r++ )
+  //create "bd" with a constructor (S. Liu)
+  bd = new CBDmulti(np1, np2, molfnames1,molfnames2, idiel);
+  
+  // start BD
+  char outfname[MAX_CHARNUM];
+  sprintf(outfname, "mfpt.%s.dat", runname);
+  ofstream fout(outfname);
+  double start = read_timer();
+  int cdock = 0;
+  for (int r=0; r<ntraj; r++)
 	{
-		// Time the run
-		double starti = read_timer(  );
-		// If a restart configuration is given, use it
-		if( bRestart )
-			bd->restartConfig( configfname );
-		// Otherwise, reset the lattice
+		double starti = read_timer();
+		
+		if(bRestart)
+			bd->restartConfig(configfname);
 		else
-			bd->resetLattice(  );		// place molecules on a lattice (S. Liu)
-		//  "runFirstDockTime" will perform the BD simulation until
-		//  one trajectory ends (S. Liu)
-		double docktime = bd->runFirstDockTime(  maxtime, runname  );
-		// Time the completed run
-		double endi = read_timer(  );
-		// If the docktime is less than the max
-		// allotted time, add it to the compounds that
-		// docked
-		if( docktime < maxtime  ) cdock++;
-		// print out the simulation time
-		fout <<r<<" "<<docktime<<"[ns]; computation time ="<<endi-starti<< endl;
+			bd->resetLattice(); //place molecules on a lattice (S. Liu)
+    //"runFirstDockTime" will perform the BD simulation until one trajectory ends (S. Liu)
+    double docktime = bd->runFirstDockTime( maxtime, runname );
+    double endi = read_timer();
+    if(docktime < maxtime ) cdock++;
+    fout <<r<<" "<<docktime<<"[ns]; computation time ="<<endi-starti<< endl;
 	}
-	
-	// Report the total docking percent
-	cout << "Total Docked "<<cdock<<" out of "<<ntraj<<" trajectories"<<endl;
-	// Cleaning up
-	CSystem::deleteConstants(  );
-	delete bd;
-	// Measuring total simulation time
-	double end = read_timer(  );
-	// Timing reports
-	cout <<"Program runtime = "<<end-start<<" s"<< endl;
-	cout <<"Program completed successfully"<<endl;
-	
-	return 0;
+  
+  cout << "Total Docked "<<cdock<<" out of "<<ntraj<<" trajectories"<<endl;
+  delete bd;
+  double end = read_timer();
+  
+  cout <<"Program runtime = "<<end-start<<" s"<< endl;
+  cout <<"Program completed successfully"<<endl;
+  return 0;
 } // end BD multi
 
 /******************************************************************/
